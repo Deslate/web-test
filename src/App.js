f0f0a3460143fa52ef59c68317cba775c1e6ed91
Corleonedele <1291683680@qq.com>
2021-01-21 22:54:44 +0800

    first-version

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <p>
+          Visit our web
+          <a
+            // style={{
+            //   marginLeft:"10px"
+            // }}
+            className="App-TeaBreak" // css in App.css and import this css file
+            href="https://tea-break.cn"
+            target="_blank" // whether open new window
+            rel="noopener noreferrer"
+            // onClick={()=>{
+            //   window.location.href="https://tea-break.cn" //as the node is button to define a funciton
+            // }}
+          >
+            Our Website
+          </a>
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +36,11 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+      <body>
+        
+        </body>>
     </div>
   );
 }
